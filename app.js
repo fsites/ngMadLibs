a9752226a5fedd41cfe15b89ac0aa5ea87081fd4
fsites <fsites521@gmail.com>
2015-05-20 18:24:07 -0700

    working version with contorller

---
+++ app.js
@@ -0,0 +1,11 @@
+angular.module('madlibs', [])
+.controller('MyCtrl', ['$scope', function($scope){
+	$scope.femaleName = '';
+	$scope.dirtyTask = '';
+	$scope.obnxiousCelebrity = '';
+	$scope.jobTitle = '';
+	$scope.celebrity = '';
+	$scope.hugeNumber = '';
+	$scope.tediousTask = '';
+	$scope.adjective = '';
+}]);
