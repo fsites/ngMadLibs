0f21e9fd3d9bbb627818938c191e4aa8f13bd01b
fsites <fsites521@gmail.com>
2015-05-21 15:34:36 -0700

    show and hide working, for validation issues

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-angular.module('madlibs', [ngMessages])
+angular.module('madlibs', ['ngMessages'])
 .controller('MyCtrl', ['$scope', function($scope){
-	$scope.femaleName = '';
-	$scope.dirtyTask = '';
-	$scope.obnxiousCelebrity = '';
-	$scope.jobTitle = '';
-	$scope.celebrity = '';
-	$scope.hugeNumber = '';
-	$scope.tediousTask = '';
-	$scope.adjective = '';
+
+	$scope.onSubmit = function (){
+		if ($scope.madlib.$valid) {
+			console.log('valid form')
+			$scope.display = 1;
+		} else {
+			console.log('go fuck yourself')
+		}
+	};
+
+	$scope.reset = function() {
+		$scope.display = 0
+		$scope.femaleName = '';
+		$scope.dirtyTask = '';
+		$scope.obnxiousCelebrity = '';
+		$scope.jobTitle = '';
+		$scope.celebrity = '';
+		$scope.hugeNumber = '';
+		$scope.tediousTask = '';
+		$scope.adjective = '';
+	};
+
+	$scope.display = 0;
+
 }]);
